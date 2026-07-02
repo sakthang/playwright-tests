1b268e6f5b845afcfc877aa3ed34c155b65d0ed2
Thangaraj <sakthi-kumaran.a.thangaraj@capgemini.com>
2026-07-02 14:28:59 +1200

    Update contact page and contact tests

--- src/page-objects/cart.page.js
+++ src/page-objects/cart.page.js
@@ -1,7 +1,6 @@
 const BasePage = require('./base.page');
 const { expect } = require('@playwright/test');
 
-
 class CartPage extends BasePage {
     constructor(page) {
     super(page);
@@ -10,29 +9,39 @@ class CartPage extends BasePage {
     this.tablerow = () => this.page.locator('.cart-items tbody tr'); 
   }
 
-  async verifySubTotal(dataToAssert)
+  async getDetailsFromCart()
   { 
      await this.waitForVisible(this.tablerow().first()); 
      const rows = await this.getRowCount(this.tablerow());//count();
      const {priceIndex, quantityIndex, subTotalIndex} = await this.getHeaderIndexes(this.tableheader());
-  let finalTotal = 0;
+     let finalTotal = 0;
+     
+    const cartItems = [];
+
      for (let i = 0; i < rows ; i++ )
           {
-   const row = this.tablerow().nth(i);
+    const row = this.tablerow().nth(i);
     
-   const productName = (await row.locator('td').first().textContent()).trim();
-
-   const price =  parseFloat((await row.locator('td').nth(priceIndex).textContent()).replace(/[^0-9.]/g, ''));
+    const productName = (await row.locator('td').first().textContent()).trim();
  
-   const quantity = parseInt(await row.locator('input').inputValue());
+    const price =  parseFloat((await row.locator('td').nth(priceIndex).textContent()).replace(/[^0-9.]/g, ''));
  
-   const subTotal = parseFloat((await row.locator('td').nth(subTotalIndex).textContent()).replace(/[^0-9.]/g, ''));
+    const quantity = parseInt(await row.locator('input').inputValue());
  
-  expect(price).toBeCloseTo(dataToAssert[productName], 2); 
-  expect(subTotal).toBeCloseTo(price*quantity, 2);
+    const subTotal = parseFloat((await row.locator('td').nth(subTotalIndex).textContent()).replace(/[^0-9.]/g, ''));
+      cartItems.push({
+      productName,
+      price,
+      quantity,
+      subTotal
+    });
      finalTotal += subTotal;
   }
- expect(finalTotal).toBeCloseTo(120.9, 2);
+ return {
+    cartItems,
+    finalTotal
+  };
+
 }
 }
 module.exports = CartPage;
--- src/tests/shoppage.spec.js
+++ src/tests/shoppage.spec.js
@@ -7,7 +7,7 @@ const CartPage = require('../page-objects/cart.page.js');
 
 const BASE_URL = "http://jupiter.cloud.planittesting.com";
 
-test('Error validation when mandatory fields not entered ', async ({ page }) => {
+test('Verify Cartpage subtotal and total for purchased items ', async ({ page }) => {
 
     const homePage = new HomePage(page);
     const shopPage = new ShopPage(page);
@@ -20,10 +20,21 @@ test('Error validation when mandatory fields not entered ', async ({ page }) => 
     await shopPage.buyProduct(productToBuy.name, productToBuy.quantity);
   }
    await homePage.goToCartPage();
-   await cartPage.verifySubTotal({
-  "Teddy Bear": 12.99,
-  "Fluffy Bunny": 9.99,
-  "Valentine Bear": 14.99
-});
-});
+   
+   const dataForAssertion = {
+    "Teddy Bear": 12.99,
+    "Fluffy Bunny": 9.99,
+    "Valentine Bear": 14.99
+   }
 
+  const { cartItems, finalTotal } = await cartPage.getDetailsFromCart();
+
+  for (const item of cartItems) {
+    expect(item.price).toBeCloseTo(
+      dataToAssert[item.productName],2);
+
+    expect(item.subTotal).toBeCloseTo(
+      item.price * item.quantity,2);
+  }
+   expect(finalTotal).toBeCloseTo(120.9, 2);
+});
